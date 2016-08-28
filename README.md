d9b7bd4eafb1cc5d95cf333aaed33bf717726b24
BrianHGrant <bhgrant8@gmail.com>
2016-08-27 19:42:02 -0700

    create new repo for portfolio site

---
+++ README.md
@@ -0,0 +1,32 @@
+# _My Portfolio_
+
+#### _Website Portfolio of Work, 5/27/2016_
+
+#### By _**Brian Grant**_
+
+## Description
+
+_This is a web portfolio detailing my background in Technology and displaying some of my coding
+projects with links to the repositories on GitHub_
+
+## Setup/Installation Requirements
+
+_Click <a href="https://BrianHGrant.github.io/my-portfolio">Here</a> to view on GitPages_
+
+## Known Bugs
+
+_No Known Issues_
+
+## Support and contact details
+
+_Contact me at bhgrant8@gmail.com_
+
+## Technologies Used
+
+_Site created using Bootstrap, CSS, and HTML
+
+### License
+
+*GPL license*
+
+Copyright (c) 2016 **_Brian H Grant_**
---
+++ js/scripts.js
@@ -0,0 +1,96 @@
+$(document).ready(function(event) {
+  $('.bio-pic').hover(
+    function() {
+      $(this).attr("src","./img/21664b48_o.jpeg");
+    }, function() {
+      $(this).attr("src","./img/my-face.jpeg");
+    }
+  )
+  $('#btn-bio').click(function(event) {
+    $('#portfolio-space').hide();
+    $('#carousel-space').hide();
+    $('#btn-bio').hide();
+    $('#btn-portfolio').show();
+    $('#bio-space').show();
+  });
+
+  $('#resume-btn-bio').click(function(event) {
+    $('#portfolio-space').hide();
+    $('#resume-space').hide();
+    $('#btn-bio').hide();
+    $('.main-window').show();
+    $('.side-image').show();
+    $('#btn-portfolio').show();
+    $('#btn-resume').show();
+    $('#bio-space').show();
+  });
+
+  $('#btn-portfolio').click(function(event) {
+    $('#btn-portfolio').hide();
+    $('#resume-space').hide();
+    $('#carousel-space').hide();
+    $('#bio-space').hide();
+    $('#btn-bio').show();
+    $('#portfolio-space').show();
+  });
+
+  $('#resume-btn-portfolio').click(function(event) {
+    $('#portfolio-space').show();
+    $('#resume-space').hide();
+    $('#btn-bio').show();
+    $('.main-window').show();
+    $('.side-image').show();
+    $('#btn-portfolio').hide();
+    $('#btn-resume').show();
+    $('#bio-space').hide();
+  });
+
+  $('#btn-resume').click(function(event) {
+    $('#portfolio-space').hide();
+    $('#carousel-space').hide();
+    $('.side-image').hide();
+    $('#btn-resume').hide();
+    $('#bio-space').hide();
+    $('#resume-space').show();
+  });
+
+  $('#resume-btn-downloadPDF').click(function(event) {
+    window.open('./pdf/BrianGrantResume.pdf');
+  })
+
+  var i = 0
+  $('#down-arrow').click(function(event) {
+    $('.portfolio-project-' + i).hide();
+    $('#portfolio-link-' + i).hide();
+    if (i < 4) {
+      i++;
+    } else {
+      i = 4;
+    };
+    if (i === 4) {
+      $("#down-arrow").hide();
+    }
+    $('#up-arrow').show();
+    $('#down-arrow').css("margin-left", "0%");
+    $('.portfolio-project-' + i).show();
+    $('#portfolio-link-' + i).show();
+  });
+
+  $('#up-arrow').click(function(event) {
+    $('.portfolio-project-' + i).hide();
+    $('#portfolio-link-' + i).hide();
+    if (i >= 0) {
+      i--;
+    } else {
+      i = 0;
+    };
+    if (i === 0) {
+      $("#up-arrow").hide();
+      $('#down-arrow').css("margin-left", "48%");
+    };
+    $('#down-arrow').show();
+    $('.portfolio-project-' + i).show();
+    $('#portfolio-link-' + i).show();
+  });
+
+});
